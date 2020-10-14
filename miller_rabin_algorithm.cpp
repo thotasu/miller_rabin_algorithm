5accbf602f95edbb1bee46f17e16e2e77057e8d3
thotasu <sumadhar@hotmail.com>
2020-10-14 19:01:36 -0400

    Add files via upload

---
+++ miller_rabin_algorithm.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <utility>
+#include <math.h>
+using namespace std;
+
+bool miller_rabin(int a, int nk, int n) {
+	if (n < 2) {
+		return false;
+	}
+	else if ((pow(a, nk) != -1) || (pow(a, nk) != 1)) {
+		return false;
+	}
+	else if ((pow(a, nk) == 1) && fmod(nk,2) == 0) {
+		return miller_rabin(a, nk / 2, n);
+	}
+	else return true;
+}
+
+bool primality_test(int n) {    // this algorithm works for n>3
+	double a = 0;
+	while (a < 2 || (fmod(a, n) == 0 && n > 4)) {
+		a = rand() % (n-2);
+	}
+	int b = 0;
+	b = fmod(pow(a, (n - 1)), n);
+	if (n < 2 || fmod(pow(a, (n - 1)), n) != 1) {
+		return false;
+	}
+	else if (fmod(n, 2) == 0) {
+		return miller_rabin(a, (n - 1)/2, n);
+	}
+	else return true;
+}
+
+int main() {
+	double p = 15;
+
+	cout << primality_test(p);
+	return 0;
+}
