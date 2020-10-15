f9675a1e10d545a762b3cfb4a892e035a3786a33
thotasu <sumadhar@hotmail.com>
2020-10-14 21:52:18 -0400

    Update miller_rabin_algorithm.cpp

--- miller_rabin_algorithm.cpp
+++ miller_rabin_algorithm.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 bool miller_rabin(int a, int nk, int n) {
-	if (n < 2) {
+	if (n < 1) {
 		return false;
 	}
 	else if ((pow(a, nk) != -1) || (pow(a, nk) != 1)) {
@@ -23,7 +23,7 @@ bool primality_test(int n) {    // this algorithm works for n>3
 	}
 	int b = 0;
 	b = fmod(pow(a, (n - 1)), n);
-	if (n < 2 || fmod(pow(a, (n - 1)), n) != 1) {
+	if (n < 2 || n%2 ==0 || fmod(pow(a, (n - 1)), n) != 1) {
 		return false;
 	}
 	else if (fmod(n, 2) == 0) {
